73ad09b05e7afca25d57febd760767b91bcd4364
IshitaBharadwaj <ishitabharadwaj04@gmail.com>
2023-02-14 15:44:05 +0530

    changed Jenkins to correct Version

--- main/PES1UG20CS648.cpp
+++ main/PES1UG20CS648.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World. This is change in task5!\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout<< "This is a change";
+  cout << "I have successfully built and run the pipeline\n";
   return 0;
 }
--- main/task5.cpp
+++
@@ -1,9 +0,0 @@
-//File: hello.cpp
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello Ishita Bharadwaj!\n";
-  cout << "This is task 5\n";
-  return 0;
-}
